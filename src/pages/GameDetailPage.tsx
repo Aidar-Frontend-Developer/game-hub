f9acd0f43beb1f7993965f0e2d009c6b842693dd
Aidar <zoomme.kzn@gmail.com>
2023-04-23 18:18:00 +0300

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
-import { Heading, Spinner } from '@chakra-ui/react';
+import { GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
+import GameTrailer from '../components/GameTrailer';
 import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
@@ -15,11 +16,16 @@ const GameDetailPage = () => {
 	if (error || !game) throw error;
 
 	return (
-		<>
-			<Heading>{game.name}</Heading>
-			<ExpandableText>{game.description_raw}</ExpandableText>
-			<GameAttributes game={game} />
-		</>
+		<SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+			<GridItem>
+				<Heading>{game.name}</Heading>
+				<ExpandableText>{game.description_raw}</ExpandableText>
+				<GameAttributes game={game} />
+			</GridItem>
+			<GridItem>
+				<GameTrailer gameId={game.id} />
+			</GridItem>
+		</SimpleGrid>
 	);
 };
 
